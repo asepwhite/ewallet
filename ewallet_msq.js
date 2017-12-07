5545f1f3dde12e7646eea787aaa230d3a76caef9
asepwhite <septriyan.akbar@gmail.com>
2017-12-07 12:41:07 +0700

    register user to databse

--- ewallet_msq.js
+++ ewallet_msq.js
@@ -1,3 +1,4 @@
+const ewallet = require('./ewallet')
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('sisdis', 'root', 'rootroot', {
   host: 'localhost',
@@ -32,7 +33,7 @@ function sendPingMessage(ch, ex)
     currTime = moment(currTime).format("YYYY-MM-DD HH:mm:ss");
     var message = '{"action":"ping","npm":"1406623064","ts":"'+currTime+'"}'
     ch.publish(ex, '', new Buffer(message));
-    console.log("SUCCES SENDING PING");
+    console.log("SUCCESS SENDING PING");
   }, 5000);
 }
 
@@ -42,7 +43,7 @@ function initPingConsumer(){
       var ex = 'EX_PING';
       ch.assertExchange(ex, 'fanout', {durable: false});
       ch.assertQueue('', {exclusive: true}, function(err, q) {
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+        console.log(" STARTING PING CONSUMER", q.queue);
         ch.bindQueue(q.queue, ex, '');
         ch.consume(q.queue, function(msg) {
           var strMessage = msg.content.toString();
@@ -62,30 +63,23 @@ function initPingConsumer(){
 }
 
 function initRegisterPublisher(routingKey, userID, name, senderID){
-  console.log("--------------------")
-  console.log(routingKey)
-  console.log(userID)
-  console.log(name)
-  console.log(senderID)
-  console.log("--------------------")
   amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var message = {};
-    message.action = "register";
-    message.user_id = userID;
-    message.nama = name;
-    message.sender_id = senderID;
-    message.type = "request";
-    var currTime = new Date(Date.now());
-    currTime = moment(currTime).format("YYYY-MM-DD HH:mm:ss");
-    message.ts = currTime;
-    message = JSON.stringify(message);
-    var ex = 'EX_REGISTER';
-    ch.assertExchange(ex, 'direct', {durable: true});
-    ch.publish(ex, routingKey, new Buffer(message));
-    console.log(" [x] Sent a message with register key %s: and message'%s'", routingKey, message);
-  });
-  // setTimeout(function() { conn.close(); process.exit(0) }, 500);
+    conn.createChannel(function(err, ch) {
+      var message = {};
+      message.action = "register";
+      message.user_id = userID;
+      message.nama = name;
+      message.sender_id = senderID;
+      message.type = "request";
+      var currTime = new Date(Date.now());
+      currTime = moment(currTime).format("YYYY-MM-DD HH:mm:ss");
+      message.ts = currTime;
+      message = JSON.stringify(message);
+      var ex = 'EX_REGISTER';
+      ch.assertExchange(ex, 'direct', {durable: true});
+      ch.publish(ex, routingKey, new Buffer(message));
+      console.log(" Sent a message with register key %s: and message'%s'", routingKey, message);
+    });
   })
 }
 
@@ -99,8 +93,20 @@ function initRegisterConsumer(){
         console.log(' [*] Waiting for logs. To exit press CTRL+C');
         ch.bindQueue(q.queue, ex, routingKey);
         ch.consume(q.queue, function(msg) {
-          console.log("BERHASIL MASUK KE CONSUMER REGISTER");
+          console.log("Reading message data");
           console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+          try{
+            var message = JSON.parse(msg)
+            ewallet.registerUser(message.user_id, message.nama).then(function(result){
+              console.log("INI ADALAH HASIL "+ result);
+            })
+          } catch(e) {
+            console.log("error parsing JSON, logging message")
+            console.log("=========")
+            console.log(msg);
+            console.log("*********")
+          }
+
         }, {noAck: true});
       });
     });
@@ -110,8 +116,8 @@ initRegisterConsumer()
 setInterval(function(){
     initRegisterPublisher('REQ_1406623064', '1406623064', 'Akbar Septriyan', '1406623064')
 }, 5000);
-
-
-initPingPublisher();
-console.log("init consumer");
-initPingConsumer();
+//
+//
+// initPingPublisher();
+// console.log("init consumer");
+// initPingConsumer();
